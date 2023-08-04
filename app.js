b374c74759b275a424261e30815b43f5870c2c20
Sarah Young <sy398@cornell.edu>
2023-08-04 13:10:08 -0400

    finished profit functionality

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const chalk = require('chalk');
 const inquirer = require("inquirer");
 const axios = require("axios");
 const yahooStockAPI = require('yahoo-stock-api').default;
+const yahooFinance = require('yahoo-finance2').default;
 
 const yahoo = new yahooStockAPI();
 // extension: profit tracker 
 let userName;
 let userCmd;
+let defaultCurr;
+
 let wallet = 0; // will be in the currency that you start with
 
 //helper to resolve animations
@@ -28,12 +31,11 @@ async function startGame() {
   // await resolveAnimations();
   // //stop the animation
   // welcomeMsg.stop();
-  console.log(chalk.blue('Hello world!'));
   const welcomeType = `Welcome. This is a CLI financial profit manager. \n`
   await typewriterEffect(welcomeType);
   const purposeType = `You can exchange foreign currencies, get real-time stock prices of indexes, and determine whether you will be at a profit or not. \n`
   await typewriterEffect(purposeType);
-  const commands = `If you want to exchange currency, type 'exchange'. \nIf you want to see the current stock prices, type 'stock'.\nIf you want to see profits, type 'profit'.\nIf you want to exit, select 'quit'\n`
+  const commands = `If you want to exchange currency, type 'exchange'. \nIf you want to see the current stock prices, type 'stock'.\nIf you want to see profits, type 'profits'.\nIf you want to exit, select 'quit'\n`
   await typewriterEffect(commands);
   const transition = `To start, please provide us some information about you, so that we can get started\n`;
   await typewriterEffect(transition);
@@ -47,6 +49,13 @@ async function userProfile() {
     message: 'Hello, please enter your name.\n'
   });
   userName = name_answer.user_name;
+
+  let curr_answer = await inquirer.prompt({
+    name: 'currency',
+    type: 'input',
+    message: `Hi ${userName}. Please enter your default currency.\n`
+  });
+  defaultCurr = curr_answer.currency.toUpperCase();
 };
 
 async function commandCenter() {
@@ -56,9 +65,9 @@ async function commandCenter() {
     let function_answer = await inquirer.prompt({
       name: 'functional_name',
       type: 'input',
-      message: `Enter any of the following commands: 'exchange', 'stock', 'profits', 'quit'\n`
+      message: `Enter any of the following commands: exchange, stock, profits, quit\n`
     });
-    userCmd = function_answer.functional_name;
+    userCmd = function_answer.functional_name.toLowerCase();
     if (userCmd == 'exchange' || userCmd == 'stock' || userCmd == 'profits' || userCmd == 'quit') {
       valid = true;
       if (userCmd == 'exchange') {
@@ -66,6 +75,9 @@ async function commandCenter() {
       }
       if (userCmd == 'stock') {
         stock();
+      }
+      if (userCmd == 'profits') {
+        profit();
       }
     } else {
       typewriterEffect(`This is not a valid input. Please try again. \n`);
@@ -84,14 +96,14 @@ async function exchange() {
     type: 'input',
     message: `What currency are you starting with?\n`
   });
-  starting = start_curr.starting;
+  starting = start_curr.starting.toUpperCase();
 
   let end_curr = await inquirer.prompt({
     name: 'ending',
     type: 'input',
     message: `What currency do you want the result to be in?\n`
   });
-  ending = end_curr.ending;
+  ending = end_curr.ending.toUpperCase();
 
   let currency_amount = await inquirer.prompt({
     name: 'amount',
@@ -99,17 +111,25 @@ async function exchange() {
     message: `What is the value you are exchanging?\n`
   });
   value = currency_amount.amount;
-  await convertForeignCurr(starting, ending, value);
+  newValue = await convertForeignCurr(starting, ending, value);
+  if (newValue != null) {
+    await typewriterEffect(`${value} ${starting} will give you ${newValue[0]} ${newValue[1]} \n`);
+    commandCenter();
+  } else {
+    exchange();
+  }
 }
 
 async function convertForeignCurr(start, end, value) {
   try {
     const response = await axios.get(`https://api.frankfurter.app/latest?amount=${value}&from=${start}&to=${end}`);
-    await typewriterEffect(`${value} ${start} will give you ${response.data.rates[end]} ${end} \n`);
-    commandCenter();
+    // await typewriterEffect(`${value} ${start} will give you ${response.data.rates[end]} ${end} \n`);
+
+    // returns the final value, and the currency 
+    return [response.data.rates[end], end]
   } catch (error) {
     await typewriterEffect(`There is an error with one of your inputs. Please try again.\n`);
-    exchange();
+    return null;
   }
 
 }
@@ -119,19 +139,76 @@ async function stock() {
   let index_curr = await inquirer.prompt({
     name: 'index',
     type: 'input',
-    message: `Which stock do you want to trade?\n`
+    message: `Which stock do you want to trade? Please enter the stock market symbol.\n`
   });
-  index = index_curr.index;
-  await stock_exchange(index)
+  index = index_curr.index.toUpperCase();
+  let value = await stock_exchange(index)
+  if (value != null) {
+    await typewriterEffect(`The value of ${value[0]}'s stock today is ${value[1]} ${value[2]}.\n`);
+  }
+  commandCenter();
 
 }
 
 async function stock_exchange(index) {
   try {
-    yahoo.getSymbol({ symbol: 'AAPL' }).then(console.log);
+    const api_response = await yahoo.getSymbol({ symbol: index })
+    // Based on previous close
+    // returns [the name of the stock exchange, previous close value, currency]
+    console.log(`Stock Exchange Response: ${[api_response.name, api_response.response.previousClose, api_response.currency]}`)
+    return [api_response.name, api_response.response.previousClose, api_response.currency];
   } catch (error) {
-    console.log(error)
+    await typewriterEffect(`There may be a typo in the symbol. Please try again.\n`);
+    return null;
   }
+}
+
+async function profit() {
+  let index;
+  let prev_stock = await inquirer.prompt({
+    name: 'index',
+    type: 'input',
+    message: `Which stock would you like to compare the price of? Please enter the stock symbol.\n`
+  });
+  index = prev_stock.index.toUpperCase();
+
+  let buy_price;
+  let buy_query = await inquirer.prompt({
+    name: 'bought',
+    type: 'input',
+    message: `What did you buy EACH ${index} for? Please note that we are using the default currency as the price you bought it for.\n`
+  });
+  buy_price = buy_query.bought;
+
+  let shares;
+  let shares_query = await inquirer.prompt({
+    name: 'share',
+    type: 'input',
+    message: `How many shares would you sell of ${index}\n`
+  });
+  shares = shares_query.share;
+
+  let current_output = await stock_exchange(index);
+  let current_value;
+  console.log(`This is the current output currency ${current_output[2]}. This is defaultCurr ${defaultCurr}`)
+  if (current_output[2] != defaultCurr) {
+    let conversion = await convertForeignCurr(defaultCurr, current_output[2], current_output[1]);
+    current_value = parseInt(conversion[0]);
+  } else {
+    current_value = parseInt(current_output[1]);
+  }
+
+
+  let profit = (current_value * shares) - (buy_price * shares);
+  let percentChange = Math.abs((profit / (buy_price * shares)) * 100).toFixed(1);
+  if (profit < 0) {
+    await typewriterEffect(`You would be at a loss, since the current value is ${current_value} ${defaultCurr}. This is a ${percentChange}% loss.`);
+  } else if (profit > 0) {
+    await typewriterEffect(`You would be at a gain, since the current value is ${current_value} ${defaultCurr}. This is a ${percentChange}% gain.`);
+  } else {
+    await typewriterEffect(`The value is the same.`)
+  }
+
 }
 async function main() {
   //invoke our game functions here
